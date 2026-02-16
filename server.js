6d07dfedaff8b1c54ca71ce241c4419a2a70e04f
Lovepreet Singh <lovepreetsingh7814893@gmail.com>
2026-02-16 15:55:27 +0530

    Updated CORS for Vercel production frontend

--- server.js
+++ server.js
@@ -18,10 +18,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-tracker-frontend-weld.vercel.app/"  
+    ],
+    credentials: true,
+  })
+);
 
 
 app.use(express.json());
